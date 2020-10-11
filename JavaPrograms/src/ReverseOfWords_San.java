a7c10ad1fe29a3bbf5232bacd030d063f5348f09
Santosh <santoshkumarnoolu@gmail.com>
2020-10-11 10:43:51 +0530

    Reverse of words code updated

---
+++ JavaPrograms/src/ReverseOfWords_San.java
@@ -0,0 +1,35 @@
+
+public class ReverseOfWords_San {
+	public static void main(String args[])
+	{
+		String strInput="Welcome to Java";
+		reverseOfWords(strInput);
+	}
+	public static void reverseOfWords(String strInput)
+	{
+		/*
+		 1. Split the word based on Spaces
+		 2. iterate each word
+		 3. Reverse characters of each word using for loop
+		 4. Join the reversed words
+		 */
+	
+	String strWords[]=strInput.split(" ");
+	String wordRev="";
+	for(String w:strWords)
+	{
+		String strCharRev="";
+		for(int i=w.length()-1;i>=0;i--)
+		{
+			strCharRev=strCharRev+w.charAt(i);
+		}
+		wordRev=wordRev+strCharRev+" ";
+		
+	}
+	System.out.println(wordRev);
+		
+		
+	}
+	
+	}
+
