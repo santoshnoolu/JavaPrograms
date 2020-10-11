733153b133954fd3721dad43066e66249ba1eb57
ManojAvs <ManojAvs@Lenovo-PC>
2020-10-11 23:04:07 +0530

    Intial Commit from Manoj

---
+++ JavaPrograms/src/com/manoj/Demo1_ReverseofWordsInSentence.java
@@ -0,0 +1,45 @@
+package com.manoj;
+
+import java.util.Scanner;
+
+public class Demo1_ReverseofWordsInSentence {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the string to be reversed");
+		String inputString = scan.nextLine();
+		scan.close();
+
+		String reverseSentence = reverseString(inputString);
+		String reverseStrings = reverseWordsofSentence(inputString);
+		System.out.println("inputString 			: " + inputString);
+		System.out.println("reverseSentence 		: " + reverseSentence);
+		System.out.println("reverseStringsOfSentence        : " + reverseStrings);
+	}
+
+	public static String reverseString(String str) {
+		String outputString = "";
+		char c[] = str.toCharArray();
+		for (int i = str.length() - 1; i >= 0; i--) {
+			outputString = outputString + c[i];
+		}
+		return outputString;
+	}
+
+	public static String reverseWordsofSentence(String str) {
+		String revSentence = "";
+		String revString = "";
+		String[] SplitSTrings = str.split(" ");
+
+		for (int j = 0; j < SplitSTrings.length; j++) {
+			revString = reverseString(SplitSTrings[j]);
+
+			if (!revSentence.isEmpty())
+				revSentence = revSentence + " ";
+
+			revSentence = revSentence + revString;
+		}
+		return revSentence;
+	}
+
+}
---
+++ JavaPrograms/src/com/manoj/Demo2_CountofChars.java
@@ -0,0 +1,29 @@
+package com.manoj;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Demo2_CountofChars {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a String");
+		String input = scan.nextLine();
+		scan.close();
+		System.out.println("Input  :" + input);
+		System.out.println("Output :" + getCountofCharsinString(input));
+	}
+
+	public static HashMap<Character, Integer> getCountofCharsinString(String inputString) {
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		char[] c = inputString.toCharArray();
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] != ' ') {
+				if (map.containsKey(c[i]))
+					map.put(c[i], map.get(c[i]) + 1);
+				else
+					map.put(c[i], 1);
+			}
+		}
+		return map;
+	}
+}
