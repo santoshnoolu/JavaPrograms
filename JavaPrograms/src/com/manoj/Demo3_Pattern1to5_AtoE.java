de159d1a9cb6e55f3f01c57f3f03403543fe1dba
ManojAvs <ManojAvs@Lenovo-PC>
2020-10-12 22:44:17 +0530

    PatternPrograms

---
+++ JavaPrograms/src/com/manoj/Demo3_Pattern1to5_AtoE.java
@@ -0,0 +1,35 @@
+package com.manoj;
+
+public class Demo3_Pattern1to5_AtoE {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		System.out.println("==============================");
+		for (int i = 5; i > 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		System.out.println("==============================");
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print((char) (j + 64));
+			}
+			System.out.println();
+		}
+		System.out.println("==============================");
+		for (int i = 5; i > 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print((char) (j + 64));
+			}
+			System.out.println();
+		}
+	}
+
+}
